e72bf13964c38a46b28e15a7b27f1ac055e2d5a1
lewapek <lewapek@gmail.com>
2017-06-10 13:33:52 +0200

    better plots

--- src/main/python3/comparison_plot.py
+++ src/main/python3/comparison_plot.py
@@ -1,4 +1,5 @@
 import argparse
+
 from matplotlib import pyplot as plot
 
 parser = argparse.ArgumentParser(description="Comparison plotter")
@@ -7,11 +8,17 @@ parser.add_argument('-c', '--comparison', dest='comparison_file', default="tmp/c
 parser.add_argument('-i', '--indexing_mode', dest='indexing_mode', default=False, required=False, help='indexing mode')
 parser.add_argument('-o', '--output', dest='output_file', required=True, help='output file')
 parser.add_argument('-t', '--title', dest='title', default='Real vs predicted', required=False, help='title')
+parser.add_argument('-x', '--xlabel', dest='xlabel', default='', required=False, help='xlabel')
+parser.add_argument('--xlabel-indexed', dest='xlabel_indexed', default=None, required=False, help='xlabel_indexed')
+parser.add_argument('-y', '--ylabel', dest='ylabel', default='', required=False, help='ylabel')
 args = parser.parse_args()
 
 input_file = open(args.comparison_file, "r")
 lines = input_file.readlines()
 input_file.close()
+
+title = args.title.replace('_', ' ')
+xlabel_indexed = args.xlabel if args.xlabel_indexed is None else args.xlabel_indexed
 
 extract_data = (
     lambda split_line: [float(split_line[0]), float(split_line[1]), float(split_line[2])]
@@ -28,7 +35,9 @@ data = sorted(data, key=lambda row: row[0])
 columns = list(zip(*data))
 real, predicted = columns[0], columns[1]
 
-plot.title(args.title)
+plot.title(title)
+plot.xlabel(args.xlabel)
+plot.ylabel(args.ylabel)
 plot.plot(real, ".", label="real", color="blue", ms=1)
 plot.plot(predicted, ".", label="predicted", color="red", ms=1)
 plot.legend(loc=0)
@@ -39,7 +48,9 @@ if args.indexing_mode:
     columns = list(zip(*data))
     real, predicted, index = columns[0], columns[1], columns[2]
     plot.figure()
-    plot.title(args.title)
+    plot.title(title)
+    plot.xlabel(xlabel_indexed)
+    plot.ylabel(args.ylabel)
     plot.plot(index, real, ".", label="real", color="blue", ms=1)
     plot.plot(index, predicted, ".", label="predicted", color="red", ms=1)
     plot.legend(loc=0)
--- src/main/python3/errors_comparison_plot.py
+++ src/main/python3/errors_comparison_plot.py
@@ -6,28 +6,37 @@ parser = argparse.ArgumentParser(description="Comparison plotter")
 parser.add_argument('-c', '--comparison', dest='comparison_file', required=True, help='comparison file')
 parser.add_argument('-o', '--output', dest='output_file', required=True, help='output file')
 parser.add_argument('-t', '--title', dest='title', default='', required=False, help='title prefix')
-parser.add_argument('--add_converter', dest='add_converter', action='store_true', help='add converter to name flag')
+parser.add_argument('-a', '--add_converter', dest='add_converter', action='store_true',
+                    help='add converter to name flag')
+parser.add_argument('--top', dest='top', default=None, required=False, help='plots on top t results')
 args = parser.parse_args()
 
 input_file = open(args.comparison_file, "r")
 lines_no_header = input_file.readlines()[1:]
 input_file.close()
 
+top = len(lines_no_header) if args.top is None else int(args.top)
+print("top = " + str(top))
+
 data = []
 for line in lines_no_header:
     split = line.split(",")
     data.append([split[0], split[1], float(split[2]), float(split[3]), float(split[4]), float(split[5]), int(split[6])])
-sorted_rmse = ["rmse", 2, sorted(data, key=lambda row: row[2])]
-sorted_mae = ["mae", 3, sorted(data, key=lambda row: row[3])]
+sorted_rmse = ["RMSE", 2, sorted(data, key=lambda row: row[2])]
+sorted_mae = ["MAE", 3, sorted(data, key=lambda row: row[3])]
 sorted_abs_div_mean = ["absDivMean", 4, sorted(data, key=lambda row: row[4])]
-sorted_relative = ["relative", 5, sorted(data, key=lambda row: row[5])]
+sorted_relative = ["relative error", 5, sorted(data, key=lambda row: row[5])]
 
 all_plots = [sorted_rmse, sorted_mae, sorted_abs_div_mean, sorted_relative]
 
 
 def convert_function(tuple_name):
     if args.add_converter:
-        return tuple_name[0].replace('_', ' ') + ' ' + tuple_name[1]
+        converter_name = tuple_name[1]
+        if converter_name == '':
+            return tuple_name[0].replace('_', ' ')
+        else:
+            return tuple_name[0].replace('_', ' ') + ' (' + converter_name + ')'
     else:
         return tuple_name[0].replace('_', ' ')
 
@@ -35,7 +44,7 @@ def convert_function(tuple_name):
 for plot_type in all_plots:
     name = plot_type[0]
     value_column = plot_type[1]
-    plot_data = plot_type[2]
+    plot_data = plot_type[2][:top]
     columns = list(zip(*plot_data))
 
     converter, regression, error = columns[0], columns[1], columns[value_column]
@@ -46,10 +55,12 @@ for plot_type in all_plots:
 
     indexes = list(range(len(error)))
     plot.figure()
-    plot.title(args.title + " " + name)
+    plot.title(args.title + ": " + name)
+    plot.xlabel(name)
     plot.barh(indexes, error, label=name)
     plot.yticks(indexes, xticks)
     plot.tight_layout()
+    plot.autoscale()
     plot.legend()
     output = args.output_file + "_" + name + ".png"
     print("output = " + output)
